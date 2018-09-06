e3465d84c33827771c42a906fea5a113480cee8d
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-09-06 09:19:24 +0530

    project created

---
+++ src/chatapp/ChatApp.java
@@ -0,0 +1,15 @@
+
+package chatapp;
+
+import chatapp.gui.MainScreen;
+
+
+public class ChatApp {
+
+    
+    public static void main(String[] args) {
+        MainScreen ms=new MainScreen();
+        ms.setVisible(true);
+    }
+    
+}
